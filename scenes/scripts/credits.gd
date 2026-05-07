fdc93d29773887e01c629a9c6032227e6f9d97a2
st0nks <peter06read@gmail.com>
2026-05-07 01:48:21 +0100

    final (hopefully) version

--- scenes/scripts/credits.gd
+++ scenes/scripts/credits.gd
@@ -1,9 +1,12 @@
 extends Control
 
+var stopwatch : float = SettingsManager.time_elapsed
+var dostopwatch : bool = SettingsManager.speedrun_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$"speedrun timer".text = "Your Final time was "+str(round(stopwatch)) + " seconds"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
